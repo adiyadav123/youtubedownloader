6823c7f7674b89abee4144d72308f8685177aef3
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2024-05-03 00:02:52 +0530

    update

--- app/page.js
+++ app/page.js
@@ -51,7 +51,7 @@ export default function Home() {
     } else {
       setBtnClicked(true);
     }
-    const url = "http://localhost:3000/api/";
+    const url = "http://ytdownloader.vercel.app/api/";
     const response = await fetch(url, {
       method: "POST",
       headers: {
